f1553517bfc0add80661d5d32473156c844c0f1d
Damien Irving <irving.damien@gmail.com>
2021-04-28 15:46:51 +1000

    Add forecast preprocessing

--- Makefile
+++ Makefile
@@ -1,24 +1,31 @@
 .PHONY: all jra awap clean help settings
 
-INIT_DATES=1989-11-01 1990-11-01 1991-11-01 1992-11-01
+INIT_DATES=1990-11-01 1991-11-01 1992-11-01 1993-11-01
 LEAD_TIME=3650
 REGION=aus
 PYTHON=/g/data/e14/dbi599/miniconda3/envs/unseen/bin/python
 DATA_DIR=/g/data/xv83/dbi599
 JRA_DATA=/g/data/xv83/ds0092/data/csiro-dcfp-jra55/surface_daily_cafe-grid.zarr/
 AWAP_DATA=/g/data/xv83/ds0092/data/csiro-dcfp-csiro-awap/rain_day_19000101-20201202_cafe-grid.zarr/
+CAFE_DATA := $(sort $(wildcard /g/data/xv83/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-199*1101/ZARR/atmos_isobaric_daily.zarr.zip))
 
 ## jra : JRA-55 precip data in forecast (i.e. initial date / lead time) format
-JRA_PR_FORECAST_FILE=${DATA_DIR}/precip_jra55_19891101-19921101_${LEAD_TIME}D_cafe-grid-${REGION}.zarr.zip
+JRA_PR_FORECAST_FILE=${DATA_DIR}/precip_jra55_19901101-19931101_${LEAD_TIME}D_cafe-grid-${REGION}.zarr.zip
 jra : ${JRA_PR_FORECAST_FILE}
 ${JRA_PR_FORECAST_FILE} : ${JRA_DATA}
 	${PYTHON} preprocess_obs.py $< TPRAT_GDS0_SFC precip $@ --n_lead_steps ${LEAD_TIME} --init_dates ${INIT_DATES} --dataset JRA-55 --no_leap_days --region ${REGION} --chunk_size 3000
 
 ## awap : AWAP precip data in forecast (i.e. initial date / lead time) format
-AWAP_PR_FORECAST_FILE=${DATA_DIR}/precip_awap_19891101-19921101_3650D_cafe-grid.zarr.zip
+AWAP_PR_FORECAST_FILE=${DATA_DIR}/precip_awap_19901101-19931101_3650D_cafe-grid.zarr.zip
 awap : ${AWAP_PR_FORECAST_FILE}
 ${AWAP_PR_FORECAST_FILE} : ${AWAP_DATA}
 	${PYTHON} preprocess_obs.py $< precip precip $@ --n_lead_steps ${LEAD_TIME} --init_dates ${INIT_DATES} --no_leap_days
+
+## cafe : preprocessing of CAFE ensemble
+CAFE_PR_ENSEMBLE_FILE=/g/data/xv83/dbi599/precip_cafe-c5-d60-pX-f6_19901101-19931101_3650D_cafe-grid.zarr.zip
+cafe : ${CAFE_PR_ENSEMBLE_FILE}
+${CAFE_PR_ENSEMBLE_FILE} :
+	${PYTHON} preprocess_forecasts.py ${CAFE_DATA} precip $@ --no_leap_days --region ${REGION}
 
 ## clean : remove all generated files
 clean :
---
+++ preprocess_forecasts.py
@@ -0,0 +1,87 @@
+"""Collect forecast data into an ensemble."""
+
+import sys
+repo_dir = sys.path[0]
+import pdb
+import argparse
+
+import git
+import numpy as np
+import xarray as xr
+import cmdline_provenance as cmdprov
+
+import myfuncs
+
+
+def open_and_clip(infile, var, region=None, no_leap_days=False):
+    """Open file and select region"""
+    
+    ds = xr.open_zarr(infile, consolidated=True, use_cftime=True)
+    
+    da = ds['precip']
+    for drop_coord in ['average_DT', 'average_T1', 'average_T2', 'zsurf', 'area']:
+        if drop_coord in da.coords:
+            da = da.drop(drop_coord)
+    
+    if no_leap_days:
+        da = da.sel(time=~((da['time'].dt.month == 2) & (da['time'].dt.day == 29)))
+
+    if region:
+        region = myfuncs.box_regions[region]
+        da = myfuncs.get_region(da, region)
+
+    lead_time = range(len(da['time']))
+    init_date = np.datetime64(da['time'].values[0].strftime('%Y-%m-%d'))
+    new_coords = {'lead_time': lead_time, 'init_date': init_date}
+    da = da.rename({'time': 'lead_time'})
+    da = da.assign_coords(new_coords)
+
+    return da
+
+
+def main(args):
+    """Run the command line program."""
+
+    datasets = []
+    for infile in args.infiles:
+        da = open_and_clip(infile, args.var,
+                           region=args.region,
+                           no_leap_days=args.no_leap_days)
+        datasets.append(da)
+    da = xr.concat(datasets, dim='init_date')
+
+    time_values = [da.get_index('init_date').shift(int(lead), 'D') for lead in da['lead_time']]
+    time_dimension = xr.DataArray(time_values,
+                                  dims={'lead_time': da['lead_time'],
+                                        'init_date': da['init_date']})
+    da = da.assign_coords({'time': time_dimension})
+    da['lead_time'].attrs['units'] = 'D'
+
+    ds = da.to_dataset()
+
+    repo = git.Repo(repo_dir)
+    repo_url = repo.remotes[0].url.split('.git')[0]
+    new_log = cmdprov.new_log(code_url=repo_url)
+    ds.attrs['history'] = new_log
+
+    ds = ds.chunk({'init_date': -1, 'lead_time': -1,
+                   'lat': -1, 'lon': -1})
+    ds.to_zarr(args.outfile, mode='w')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+                                     
+    parser.add_argument("infiles", type=str, nargs='*', help="Input files")
+    parser.add_argument("var", type=str, help="Variable name")
+    parser.add_argument("outfile", type=str, help="Output file")
+    
+    parser.add_argument("--region", type=str, choices=myfuncs.box_regions.keys(),
+                        help="Select spatial region from data")
+    parser.add_argument("--no_leap_days", action="store_true", default=False,
+                        help="Remove leap days from time series [default=False]")
+
+    args = parser.parse_args()
+    main(args)
+

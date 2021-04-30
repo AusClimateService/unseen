7064f25a153be996b1469f462adc0eb14960c6dd
Damien Irving <irving.damien@gmail.com>
2021-04-30 16:19:27 +1000

    Add bias correction

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
-.PHONY: all jra awap clean help settings
+.PHONY: all jra awap cafe awap-bias clean help settings
 
 INIT_DATES=1990-11-01 1991-11-01 1992-11-01 1993-11-01
 LEAD_TIME=3650
 REGION=aus
+BIAS_METHOD=additive
 PYTHON=/g/data/e14/dbi599/miniconda3/envs/unseen/bin/python
 DATA_DIR=/g/data/xv83/dbi599
 JRA_DATA=/g/data/xv83/ds0092/data/csiro-dcfp-jra55/surface_daily_cafe-grid.zarr/
@@ -27,15 +28,22 @@ cafe : ${CAFE_PR_ENSEMBLE_FILE}
 ${CAFE_PR_ENSEMBLE_FILE} :
 	${PYTHON} preprocess_forecasts.py ${CAFE_DATA} precip $@ --no_leap_days --region ${REGION}
 
+## awap-bias : bias corrected CAFE data using AWAP
+CAFE_PR_BIAS_FILE=/g/data/xv83/dbi599/precip_cafe-c5-d60-pX-f6_awap-additive-correction_19901101-19931101_3650D_cafe-grid.zarr.zip
+awap-bias : ${CAFE_PR_BIAS_FILE}
+${CAFE_PR_BIAS_FILE} : ${CAFE_PR_ENSEMBLE_FILE} ${AWAP_PR_FORECAST_FILE}
+	${PYTHON} bias_correction.py $< $(word 2,$^) precip ${BIAS_METHOD} $@
+
 ## clean : remove all generated files
 clean :
 	rm ${JRA_PR_FORECAST_FILE} ${AWAP_PR_FORECAST_FILE}
 
 ## settings : show variables' values
 settings :
-	@echo INIT_DATES: $(INIT_DATES)
-	@echo LEAD_TIME: $(LEAD_TIME)
-	@echo REGION: $(REGION)
+	@echo INIT_DATES: ${INIT_DATES}
+	@echo LEAD_TIME: ${LEAD_TIME}
+	@echo REGION: ${REGION}
+	@echo BIAS_METHOD: ${BIAS_METHOD}
 
 ## help : show this message
 help :
---
+++ bias_correction.py
@@ -0,0 +1,59 @@
+"""Bias correction."""
+
+import sys
+repo_dir = sys.path[0]
+import pdb
+import argparse
+
+import git
+import xarray as xr
+import cmdline_provenance as cmdprov
+
+import myfuncs
+
+
+def main(args):
+    """Run the command line program."""
+
+    ds_model = xr.open_zarr(args.model_file)
+    da_model = ds_model[args.var]
+    model_clim = da_model.mean(['ensemble', 'init_date'])
+    model_clim = model_clim.transpose('lead_time', 'lat', 'lon')
+    
+    ds_obs = xr.open_zarr(args.obs_file)
+    da_obs = ds_obs[args.var]
+    obs_clim = da_obs.mean('init_date')
+    obs_clim = obs_clim.transpose('lead_time', 'lat', 'lon')
+    
+    if args.method == 'additive':
+        bias = model_clim - obs_clim
+        corrected_model = da_model - bias
+    elif args.method == 'multiplicative':
+        bias = model_clim / obs_clim
+        corrected_model = da_model / bias
+    
+    ds = corrected_model.to_dataset()
+
+    repo = git.Repo(repo_dir)
+    repo_url = repo.remotes[0].url.split('.git')[0]
+    new_log = cmdprov.new_log(code_url=repo_url)
+    ds.attrs['history'] = new_log
+
+#    ds = ds.chunk({'init_date': 1, 'lead_time': 50})
+    ds.to_zarr(args.outfile, mode='w')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+                                     
+    parser.add_argument("model_file", type=str, help="Model file")
+    parser.add_argument("obs_file", type=str, help="Observations file")
+    parser.add_argument("var", type=str, help="Variable name")
+    parser.add_argument("method", type=str,
+                        choices=('multiplicative', 'additive'),
+                        help="Bias correction method")
+    parser.add_argument("outfile", type=str, help="Output file")
+
+    args = parser.parse_args()
+    main(args)

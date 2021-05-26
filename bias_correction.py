1b541d60a30cd560f1e1b011143fa97a5ff4f4c7
Damien Irving <irving.damien@gmail.com>
2021-05-26 10:38:38 +1000

    Split bias correction into functions

--- bias_correction.py
+++ bias_correction.py
@@ -16,26 +16,60 @@ import myfuncs
 # https://github.com/dougiesquire/hydro_tasmania/blob/main/2020/myfuncs.py#L242
 
 
-def main(args):
+def get_bias(da_model, da_obs, method):
+    """Calculate model bias.
+
+    Args:
+      da_model (xarray DataArray) : Model data
+      da_obs (xarray DataArray) : Observational data
+      method (str) : Bias removal method
+    """
+
+    model_clim = da_model.mean(['ensemble', 'init_date'])
+    obs_clim = da_obs.mean('init_date')
+    if method == 'additive':
+        bias = model_clim - obs_clim
+    elif method == 'multiplicative':
+        bias = model_clim / obs_clim
+    else:
+        raise ValueError('Invalid bias removal method')
+    bias.attrs['bias_correction_method'] = method
+
+    return bias
+
+
+def remove_bias(da, bias, method):
+    """Remove model bias.
+
+    Args:
+      da (xarray DataArray) : Data
+      bias (xarray DataArray) : Bias
+      method (str) : Bias removal method
+    """
+
+    if method == 'additive':
+        da_corrected = da - bias
+    elif method == 'multiplicative':
+        da_corrected = da / bias
+    else:
+        raise ValueError('Invalid bias removal method')
+    da_corrected.attrs['bias_correction_method'] = bias.attrs['bias_correction_method']
+
+    return da_corrected
+
+
+def _main(args):
     """Run the command line program."""
 
     ds_model = xr.open_zarr(args.model_file)
     da_model = ds_model[args.var]
-    model_clim = da_model.mean(['ensemble', 'init_date'])
-    
     ds_obs = xr.open_zarr(args.obs_file)
     da_obs = ds_obs[args.var]
-    obs_clim = da_obs.mean('init_date')
 
-    if args.method == 'additive':
-        bias = model_clim - obs_clim
-        corrected_model = da_model - bias
-    elif args.method == 'multiplicative':
-        bias = model_clim / obs_clim
-        corrected_model = da_model / bias
+    bias = get_bias(da_model, da_obs, args.method) 
+    da_corrected = remove_bias(da_model, bias, args.method)
     
-    ds = corrected_model.to_dataset()
-
+    ds = da_corrected.to_dataset()
     repo = git.Repo(repo_dir)
     repo_url = repo.remotes[0].url.split('.git')[0]
     new_log = cmdprov.new_log(code_url=repo_url)
@@ -48,7 +82,6 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-                                     
     parser.add_argument("model_file", type=str, help="Model file")
     parser.add_argument("obs_file", type=str, help="Observations file")
     parser.add_argument("var", type=str, help="Variable name")
@@ -56,6 +89,5 @@ if __name__ == '__main__':
                         choices=('multiplicative', 'additive'),
                         help="Bias correction method")
     parser.add_argument("outfile", type=str, help="Output file")
-
     args = parser.parse_args()
-    main(args)
+    _main(args)

ec9db611936687beb31b0c6227bac154a6023313
Damien Irving <irving.damien@gmail.com>
2021-08-09 14:30:05 +1000

    Update AWAP to 2020

--- config/workflow_ag.mk
+++ config/workflow_ag.mk
@@ -12,7 +12,7 @@ TIME_FREQ=A-DEC
 TIME_AGG=mean
 
 BIAS_METHOD=additive
-BASE_PERIOD=1990-01-01 2019-12-31
+BASE_PERIOD=1990-01-01 2020-12-31
 
 IO_OPTIONS=--variables ${VAR} --spatial_coords -44 -11 113 154 --shapefile ${SHAPEFILE} --shp_header ${SHP_HEADER} --combine_shapes --spatial_agg ${SPATIAL_AGG} --time_freq ${TIME_FREQ} --time_agg ${TIME_AGG} --complete_time_agg_periods --units ${UNITS}
 
@@ -20,9 +20,9 @@ FCST_DATA := $(sort $(wildcard /g/data/xv83/dcfp/CAFE-f6/c5-d60-pX-f6-??[9,0,1,2
 FCST_METADATA=config/dataset_cafe_daily.yml
 FCST_ENSEMBLE_FILE=/g/data/xv83/dbi599/${VAR}_cafe-c5-d60-pX-f6_19900501-20201101_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}.zarr.zip
 
-OBS_DATA=/g/data/xv83/ds0092/data/csiro-dcfp-csiro-awap/rain_day_19000101-20201202_cafe-grid.zarr/
+OBS_DATA=/g/data/xv83/observations/AWAP/rain_day_19000101-20210425_cafe-grid.zarr.zip
 OBS_METADATA=config/dataset_awap_daily.yml
-OBS_FORECAST_FILE=/g/data/xv83/dbi599/${VAR}_awap_1900-2019_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}.zarr.zip
+OBS_FORECAST_FILE=/g/data/xv83/dbi599/${VAR}_awap_1900-2020_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}.zarr.zip
 
 FCST_BIAS_FILE=/g/data/xv83/dbi599/${VAR}_cafe-c5-d60-pX-f6_19900501-20201101_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}_bias-corrected-awap-${BIAS_METHOD}.zarr.zip
 

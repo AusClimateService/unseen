9966a3c18b62ffa0f5556b0fe58883265bac82f0
Damien Irving <irving.damien@gmail.com>
2021-10-07 13:15:28 +1100

    Script for reanalysis hottest day plot

--- projects/seatac/config_seatac.mk
+++ projects/seatac/config_seatac.mk
@@ -9,6 +9,8 @@ TIME_AGG=max
 CONFIG_DIR=/home/599/dbi599/unseen/config
 SEATAC_DIR=/g/data/xv83/dbi599/seatac
 
+DASK_CONFIG=${CONFIG_DIR}/dask_local.yml
+
 IO_OPTIONS=--variables ${VAR} --spatial_coords 47.45 -122.31 --time_freq ${TIME_FREQ} --time_agg ${TIME_AGG} --complete_time_agg_periods --units ${UNITS}
 
 FCST_DATA := $(sort $(wildcard /g/data/xv83/dcfp/CAFE-f6/c5-d60-pX-f6-??[9,0,1,2]*/atmos_isobaric_daily.zarr.zip))
@@ -18,9 +20,13 @@ FCST_ENSEMBLE_FILE=${SEATAC_DIR}/${VAR}_cafe-c5-d60-pX-f6_19900501-20201101_${TI
 OBS_FILE=${SEATAC_DIR}/tasmax_GHCNv3_SeaTac_daily_1948-2021.nc
 OBS_CONFIG=${CONFIG_DIR}/dataset_knmi_daily.yml
 
-TXX_HISTOGRAM_PLOT=${SEATAC_DIR}/tasmax_histogram_seatac.pdf
+REANALYSIS_HGT_FILE=${SEATAC_DIR}/h500_ERA5_hourly_2021-06-28.grib
+REANALYSIS_TAS_FILE=${SEATAC_DIR}/tas_ERA5_hourly_2021-06-28.grib
 
-DASK_CONFIG=${CONFIG_DIR}/dask_local.yml
+TXX_HISTOGRAM_PLOT=${SEATAC_DIR}/tasmax_histogram_seatac.pdf
+REANALYSIS_HOT_DAY_PLOT=${SEATAC_DIR}/seatac_2021-06-28_era5.pdf
+
+
 
 
 
---
+++ projects/seatac/plot_reanalysis_hottest_day.py
@@ -0,0 +1,84 @@
+"""Plot hottest SeaTac day in observations"""
+
+import pdb
+import sys
+script_dir = sys.path[0]
+repo_dir = '/'.join(script_dir.split('/')[:-2])
+module_dir = repo_dir + '/unseen'
+sys.path.insert(1, module_dir)
+
+import argparse
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import xarray as xr
+
+import fileio
+import general_utils
+
+
+def plot_usa(da_tasmax, da_h500, outfile, metadata_key, command_log, data_source):
+    """USA plot"""
+    
+    assert data_source in ['Observations', 'Model']
+
+    fig = plt.figure(figsize=[12,5])
+    map_proj = ccrs.LambertConformal(central_longitude=262.5, central_latitude=38.5, standard_parallels=[38.5, 38.5])
+    h500_levels = np.arange(5000, 6300, 50)
+    
+    ax = fig.add_subplot(111, projection=map_proj)
+    
+    da_tasmax.plot(ax=ax,
+                   transform=ccrs.PlateCarree(),
+                   cmap=plt.cm.hot_r,
+                   vmin=10, vmax=52,
+                   cbar_kwargs={'label': 'maximum temperature (C)'}) #alpha=0.7
+    
+    lines = da_h500.plot.contour(ax=ax,
+                                 transform=ccrs.PlateCarree(),
+                                 levels=h500_levels,
+                                 colors=['0.1'])
+    ax.clabel(lines, colors=['0.1'], manual=False, inline=True)
+        
+    ax.plot(-122.31 + 360, 47.45, 'bo', transform=ccrs.PlateCarree())
+    ax.coastlines()
+    ax.set_extent([-140, -60, 20, 70])
+    ax.gridlines(linestyle='--')
+    ax.set_title(f'Hottest day: {data_source}')
+    plt.savefig(outfile, metadata={metadata_key: command_log}, bbox_inches='tight', facecolor='white')
+
+
+def _main(args):
+    """Run the command line program."""
+
+    general_utils.set_plot_params(args.plotparams)
+    
+    ds_hgt = xr.open_dataset(args.hgt_file, engine='cfgrib')
+    da_h500 = ds_hgt['z'].mean('time')
+    da_h500 = da_h500 / 9.80665
+
+    ds_tas = xr.open_dataset(args.tas_file, engine='cfgrib')
+    da_tasmax = ds_tas['t2m'].max('time')
+    da_tasmax = da_tasmax - 273.15
+
+    new_log = fileio.get_new_log(repo_dir=repo_dir)
+    metadata_key = fileio.image_metadata_keys[args.outfile.split('.')[-1]]
+    plot_usa(da_tasmax, da_h500, args.outfile, metadata_key, new_log, 'Observations')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument("hgt_file", type=str, help="geopotential height file")
+    parser.add_argument("tas_file", type=str, help="temperature file")
+    parser.add_argument("outfile", type=str, help="output file")
+    
+    parser.add_argument('--plotparams', type=str, default=None,
+                        help='matplotlib parameters (YAML file)')
+    
+    args = parser.parse_args()
+    _main(args)

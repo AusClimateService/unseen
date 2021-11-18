b550a508ef854a1a92b517d8ff77aee79a433bf7
Damien Irving <irving.damien@gmail.com>
2021-11-18 16:23:56 +1100

    Run workflow for all 4 regions

--- projects/ag/workflow_ag.mk
+++ projects/ag/workflow_ag.mk
@@ -4,7 +4,8 @@ VAR=pr
 UNITS=${VAR}=mm/day
 
 SHAPEFILE=/home/599/dbi599/unseen/projects/ag/wheat_sheep.zip
-REGION_NAME=wheat-sheep
+REGION=wheat-sheep
+SUB_REGION=all
 SHP_HEADER=region
 SPATIAL_AGG=mean
 
@@ -24,17 +25,17 @@ CLIM_IO_OPTIONS=${GENERAL_IO_OPTIONS} ${TIME_IO_OPTIONS} --climatology 1990-01-0
 FCST_DATA := $(sort $(wildcard /g/data/xv83/dcfp/CAFE-f6/c5-d60-pX-f6-*/atmos_isobaric_daily.zarr.zip))
 FCST_METADATA=config/dataset_cafe_daily.yml
 FCST_CLIMATOLOGY=/g/data/xv83/dbi599/ag/${VAR}_cafe-c5-d60-pX-f6_1990-2019_annual-climatology.zarr.zip
-FCST_ENSEMBLE_FILE=/g/data/xv83/dbi599/ag/${VAR}_cafe-c5-d60-pX-f6_19811101-20201101_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}.zarr.zip
+FCST_ENSEMBLE_FILE=/g/data/xv83/dbi599/ag/${VAR}_cafe-c5-d60-pX-f6_19811101-20201101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}.zarr.zip
 
 OBS_DATA=/g/data/ia39/agcd/post-processed/data/agcd_v2_precip_total_cafe-grid_monthly_1900-2020.zarr.zip
 OBS_METADATA=config/dataset_agcd_monthly.yml
-OBS_PROCESSED_FILE=/g/data/xv83/dbi599/ag/${VAR}_agcd_1900-2019_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}.zarr.zip
+OBS_PROCESSED_FILE=/g/data/xv83/dbi599/ag/${VAR}_agcd_1900-2019_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}.zarr.zip
 
-FCST_BIAS_FILE=/g/data/xv83/dbi599/ag/${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.zarr.zip
-SIMILARITY_FILE=/g/data/xv83/dbi599/ag/ks-test_${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.zarr.zip
+FCST_BIAS_FILE=/g/data/xv83/dbi599/ag/${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.zarr.zip
+SIMILARITY_FILE=/g/data/xv83/dbi599/ag/ks-test_${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.zarr.zip
 
-INDEPENDENCE_PLOT=/g/data/xv83/dbi599/ag/independence-test_${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION_NAME}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}_region-all.png
-INDEPENDENCE_OPTIONS=--spatial_selection region=all --lead_time_increment 1
+INDEPENDENCE_PLOT=/g/data/xv83/dbi599/ag/independence-test_${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SUB_REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.png
+INDEPENDENCE_OPTIONS=--spatial_selection region=${SUB_REGION} --lead_time_increment 1
 
 DASK_CONFIG=config/dask_local.yml
 

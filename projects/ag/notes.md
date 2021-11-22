f45458ef2d40325895d16cf9615c31d1b316425c
Damien Irving <irving.damien@gmail.com>
2021-11-22 16:36:21 +1100

    Clean up ag output files

---
+++ projects/ag/notes.md
@@ -0,0 +1,20 @@
+Rank (from driest to wettest) in the observational record (1900-2019)
+and return period (rp) in the 34,272 sample model ensemble
+for each region and wheat import year.
+
+|      | all                 | south-west        | south-east        | north-east         |
+| ---  | ---                 | ---               | ---               | ---                |
+| 2019 | rank 1 (2142 years) | rank 1 (40 years) | rank 6 (18 years) | rank 1 (685 years) | 
+| 2002 | rank 2 (51 years)   | rank 8 (14 years) | rank 8 (16 years) | rank 6 (13 years)  |
+| 1994 | rank 4 (31 years)   | rank 4 (27 years) | rank 10 (10 years)| rank 9 (8 years)   |
+| 2006 | rank 7 (16 years)   | rank 46 (2 years) | rank 3 (22 years) | rank 7 (11 years)  |
+
+Observational record, inter-region correlations:
+- south-east vs north-east annual rainfall: 0.67
+- south-east vs south-west annual rainfall: 0.45
+- north-east vs south-west annual rainfall: 0.15
+
+So the special features of 2019 was the exceptionally low rainfall in the north-east coupled with record-low rainfall in the south-west also (rainfall in these two regions isn't correlated).
+
+Note that north-east rainfall (617 mm/year over 8 grid cells) dominates the wheat-sheep total
+(south-west and south-east are 412 mm/year and 373 mm/year respectively over 6 grid cells each).
--- projects/ag/workflow_ag.mk
+++ projects/ag/workflow_ag.mk
@@ -23,17 +23,17 @@ OBS_IO_OPTIONS=${FCST_IO_OPTIONS} --input_freq M
 
 FCST_DATA := $(sort $(wildcard /g/data/xv83/dcfp/CAFE-f6/c5-d60-pX-f6-*/atmos_isobaric_daily.zarr.zip))
 FCST_METADATA=config/dataset_cafe_daily.yml
-FCST_CLIMATOLOGY=/g/data/xv83/dbi599/ag/${VAR}_cafe-c5-d60-pX-f6_1990-2019_annual-climatology.zarr.zip
-FCST_ENSEMBLE_FILE=/g/data/xv83/dbi599/ag/${VAR}_cafe-c5-d60-pX-f6_19811101-20201101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}.zarr.zip
+FCST_CLIMATOLOGY=/g/data/xv83/dbi599/ag/data/${VAR}_cafe-c5-d60-pX-f6_1990-2019_annual-climatology.zarr.zip
+FCST_ENSEMBLE_FILE=/g/data/xv83/dbi599/ag/data/${VAR}_cafe-c5-d60-pX-f6_19811101-20201101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}.zarr.zip
 
 OBS_DATA=/g/data/ia39/agcd/post-processed/data/agcd_v2_precip_total_cafe-grid_monthly_1900-2020.zarr.zip
 OBS_METADATA=config/dataset_agcd_monthly.yml
-OBS_PROCESSED_FILE=/g/data/xv83/dbi599/ag/${VAR}_agcd_1900-2019_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}.zarr.zip
+OBS_PROCESSED_FILE=/g/data/xv83/dbi599/ag/data/${VAR}_agcd_1900-2019_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}.zarr.zip
 
-FCST_BIAS_FILE=/g/data/xv83/dbi599/ag/${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.zarr.zip
-SIMILARITY_FILE=/g/data/xv83/dbi599/ag/ks-test_${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.zarr.zip
+FCST_BIAS_FILE=/g/data/xv83/dbi599/ag/data/${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.zarr.zip
+SIMILARITY_FILE=/g/data/xv83/dbi599/ag/data/ks-test_${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.zarr.zip
 
-INDEPENDENCE_PLOT=/g/data/xv83/dbi599/ag/independence-test_${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SUB_REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.png
+INDEPENDENCE_PLOT=/g/data/xv83/dbi599/ag/figures/independence-test_${VAR}_cafe-c5-d60-pX-f6_19900501-20191101_${TIME_FREQ}-${TIME_AGG}_${REGION}-${SUB_REGION}-${SPATIAL_AGG}_bias-corrected-agcd-${BIAS_METHOD}.png
 INDEPENDENCE_OPTIONS=--spatial_selection region=${SUB_REGION} --lead_time_increment 1
 
 DASK_CONFIG=config/dask_local.yml

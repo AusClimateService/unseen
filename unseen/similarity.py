a6f004a25ba370090a6dbe4ce8bc79e2810e0142
Damien Irving <irving.damien@gmail.com>
2022-12-09 17:07:55 +1100

    Add Anderson-Darling test

--- unseen/similarity.py
+++ unseen/similarity.py
@@ -11,7 +11,7 @@ from . import fileio
 from . import general_utils
 
 
-def univariate_ks_test(
+def similarity_tests(
     fcst,
     obs,
     var,
@@ -20,7 +20,7 @@ def univariate_ks_test(
     ensemble_dim="ensemble",
     time_dim="time",
 ):
-    """Univariate KS test.
+    """Perform a series of similarity tests.
 
     Parameters
     ----------
@@ -61,25 +61,40 @@ def univariate_ks_test(
     obs_stacked = obs.rename({time_dim: "sample"})
     obs_stacked = obs_stacked.chunk({"sample": -1})
 
-    ks_distances = []
-    pvals = []
+    ks_statistics = []
+    ks_pvals = []
+    ad_statistics = []
+    ad_pvals = []
     for lead_time in fcst_stacked[lead_dim].values:
         fcst_data = fcst_stacked.sel({lead_dim: lead_time})
         if not np.isnan(fcst_data[var].values).all():
-            ks = xstatstests.ks_2samp_1d(obs_stacked, fcst_data, "sample")
-            ks = ks.rename({"statistic": "ks"})
-            ks = ks.rename({"pvalue": "pval"})
-            ks_distances.append(ks["ks"])
-            pvals.append(ks["pval"])
+            ks = xstatstests.ks_2samp_1d(obs_stacked, fcst_data, dim="sample")
+            ks = ks.rename({"statistic": "ks_statistic"})
+            ks = ks.rename({"pvalue": "ks_pval"})
+            ks_statistics.append(ks["ks_statistic"])
+            ks_pvals.append(ks["ks_pval"])
+            ad = xstatstests.anderson_ksamp(obs_stacked, fcst_data, dim="sample")
+            ad = ad.rename({"statistic": "ad_statistic"})
+            ad = ad.rename({"pvalue": "ad_pval"})
+            ad_statistics.append(ad["ad_statistic"])
+            ad_pvals.append(ad["ad_pval"])
 
-    ks_distances = xr.concat(ks_distances, lead_dim)
-    pvals = xr.concat(pvals, lead_dim)
-    ds = xr.merge([ks_distances, pvals])
+    ks_statistics = xr.concat(ks_statistics, lead_dim)
+    ks_pvals = xr.concat(ks_pvals, lead_dim)
+    ad_statistics = xr.concat(ad_statistics, lead_dim)
+    ad_pvals = xr.concat(ad_pvals, lead_dim)
 
-    ds["ks"].attrs = {"long_name": "kolmogorov_smirnov_statistic"}
-    ds["pval"].attrs = {
+    ds = xr.merge([ks_statistics, ks_pvals, ad_statistics, ad_pvals])
+
+    ds["ks_statistic"].attrs = {"long_name": "kolmogorov_smirnov_statistic"}
+    ds["ks_pval"].attrs = {
         "long_name": "p_value",
-        "note": "If p < 0.05 reject the null hypothesis that the samples are from different populations",
+        "note": "If p > 0.05 cannot reject the null hypothesis that the samples are drawn from the same population",
+    }
+    ds["ad_statistic"].attrs = {"long_name": "anderson_darling_statistic"}
+    ds["ad_pval"].attrs = {
+        "long_name": "p_value",
+        "note": "If p > 0.05 cannot reject the null hypothesis that the samples are drawn from the same population"
     }
 
     return ds
@@ -161,7 +176,7 @@ def _main():
         time_slice = general_utils.date_pair_to_time_slice(args.reference_time_period)
         ds_obs = ds_obs.sel({args.time_dim: time_slice})
 
-    ds_similarity = univariate_ks_test(
+    ds_similarity = similarity_tests(
         ds_fcst,
         ds_obs,
         args.var,
